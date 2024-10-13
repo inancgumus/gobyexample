3b675853681d8f7d53a576c41909c9ff14159cbc
İnanç Gümüş <m@inanc.io>
2024-10-13 16:19:54 -0400

    Listing 3.2 Testing the url package

---
+++ testing/url/url_test.go
@@ -0,0 +1,18 @@
+package url
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	const uri = "https://go.dev/play"
+
+	got, err := Parse(uri)
+	if err != nil {
+		t.Fatalf("Parse(%q) err = %q, want <nil>", uri, err)
+	}
+	want := &URL{
+		Scheme: "https", Host: "go.dev", Path: "play",
+	}
+	if *got != *want {
+		t.Errorf("Parse(%q)\ngot  %#v\nwant %#v", uri, got, want)
+	}
+}
