df46b5692b4fb8073d8ef1eadee5b931941587b5
İnanç Gümüş <m@inanc.io>
2024-10-13 00:18:31 -0400

    Listing 2.5 Passing arrays to functions with pointers

--- basics/arrays/array.go
+++ basics/arrays/array.go
@@ -4,10 +4,10 @@ const resolution8K = 7_680 * 4_320 * 3
 
 func main() {
 	selfie := [resolution8K]byte{ /* ..color data.. */ }
-	invertColors(selfie)
+	invertColors(&selfie)
 }
 
-func invertColors(colors [resolution8K]byte) {
+func invertColors(colors *[resolution8K]byte) {
 	for i := range colors {
 		colors[i] = 255 - colors[i]
 	}
