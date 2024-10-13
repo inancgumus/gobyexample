f6809de83d515e528c4d73ace5f85ff3df91e8e5
İnanç Gümüş <m@inanc.io>
2024-10-13 11:19:49 -0400

    Listing 2.15 Using a struct value

--- basics/server/main.go
+++ basics/server/main.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fileServer := server{url: "file"}
+	fileServer := &server{url: "file"}
+	fileServer.check()
 
-	fmt.Printf(
-		"url: %s response time: %d\n",
-		fileServer.url, fileServer.responseTime,
-	)
+	fmt.Printf("is slow? %t\n", fileServer.slow())
 }
