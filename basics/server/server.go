3ee3a352e10cbcf044c5717a61927ace704ab830
İnanç Gümüş <m@inanc.io>
2024-10-13 11:18:34 -0400

    Listing 2.14 Attaching methods to the server type

--- basics/server/server.go
+++ basics/server/server.go
@@ -6,3 +6,11 @@ type server struct {
 	url          string
 	responseTime time.Duration
 }
+
+func (s *server) check() {
+	s.responseTime = 3 * time.Second
+}
+
+func (s *server) slow() bool {
+	return s.responseTime > 2*time.Second
+}
