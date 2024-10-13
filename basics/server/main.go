8e98e00f096331f563206d9b22a8cb62075d5a0d
İnanç Gümüş <m@inanc.io>
2024-10-13 11:20:32 -0400

    Listing 2.16 Methods with value receivers

--- basics/server/main.go
+++ basics/server/main.go
@@ -2,9 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
-	fileServer := &server{url: "file"}
-	fileServer.check()
+type usage float64
 
-	fmt.Printf("is slow? %t\n", fileServer.slow())
+func (u usage) high() bool     { return u >= 0.95 }
+func (u usage) set(to float64) { u = usage(to) }
+
+// correct set should be:
+// func (u usage) set(to float64) usage { return usage(to) }
+
+func main() {
+	cpu := usage(0.99)
+	fmt.Println("high usage?", cpu.high()) // ..true
+
+	cpu.set(0.7) // cpu is still 0.99
+	// correct usage:
+	// cpu = cpu.set(0.7)
+	fmt.Println("high usage?", cpu.high()) // ..true
 }
