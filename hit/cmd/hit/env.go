16c3e81d81159631bc06b6c1e34519451b149b23
İnanç Gümüş <m@inanc.io>
2024-10-13 22:48:19 -0400

    Listing 5.3 Implementing the flag value parsers

--- hit/cmd/hit/env.go
+++ hit/cmd/hit/env.go
@@ -1,8 +1,27 @@
 package main
 
+import "strconv"
+
 type config struct {
 	url string // url to send requests
 	n   int    // n is the number of requests
 	c   int    // c is the concurrency level
 	rps int    // rps is the requests per second
+}
+
+type parseFunc func(string) error
+
+func stringVar(p *string) parseFunc {
+	return func(s string) error {
+		*p = s
+		return nil
+	}
+}
+
+func intVar(p *int) parseFunc {
+	return func(s string) error {
+		var err error
+		*p, err = strconv.Atoi(s)
+		return err
+	}
 }
