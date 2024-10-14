80559927a27e6d9519e8233b1f7e6aed764f0d81
İnanç Gümüş <m@inanc.io>
2024-10-14 14:25:49 -0400

    Listing 6.6 Declaring the testRun function

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -7,3 +7,14 @@ type testEnv struct {
 	stdout bytes.Buffer
 	stderr bytes.Buffer
 }
+
+func testRun(args ...string) (*testEnv, error) {
+	var t testEnv
+	t.env = env{
+		args:   append([]string{"hit"}, args...),
+		stdout: &t.stdout,
+		stderr: &t.stderr,
+		dryRun: true,
+	}
+	return &t, run(&t.env)
+}
