2781d86bfcf0be72ef052d70546094d2022d4aeb
İnanç Gümüş <m@inanc.io>
2024-10-14 14:27:00 -0400

    Listing 6.7 CLI tests

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "bytes"
+import (
+	"bytes"
+	"testing"
+)
 
 type testEnv struct {
 	env    env
@@ -17,4 +20,31 @@ func testRun(args ...string) (*testEnv, error) {
 		dryRun: true,
 	}
 	return &t, run(&t.env)
+}
+
+func TestRunValidInput(t *testing.T) {
+	t.Parallel()
+
+	e, err := testRun("http://go.dev")
+	if err != nil {
+		t.Fatalf("got %q;\nwant nil err", err)
+	}
+	if n := e.stdout.Len(); n == 0 {
+		t.Errorf("stdout = 0 bytes; want >0")
+	}
+	if n, out := e.stderr.Len(), e.stderr.String(); n != 0 {
+		t.Errorf("stderr = %d bytes; want 0; stderr:\n%s", n, out)
+	}
+}
+
+func TestRunInvalidInput(t *testing.T) {
+	t.Parallel()
+
+	e, err := testRun("-c=2", "-n=1", "invalid-url")
+	if err == nil {
+		t.Fatalf("got nil; want err")
+	}
+	if n := e.stderr.Len(); n == 0 {
+		t.Error("stderr = 0 bytes; want >0")
+	}
 }
