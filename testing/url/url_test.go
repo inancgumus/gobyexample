8c9613dbe690cb60cf8e9fd9fb577fadbb65c494
İnanç Gümüş <m@inanc.io>
2024-10-13 20:24:58 -0400

    Listing 4.6 Testing String with an empty URL

--- testing/url/url_test.go
+++ testing/url/url_test.go
@@ -24,7 +24,7 @@ func TestURLString(t *testing.T) {
 		want string
 	}{
 		{name: "nil", uri: nil, want: ""},
-		/* we'll add more test cases soon */
+		{name: "empty", uri: &URL{}, want: ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
