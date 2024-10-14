737f21ab3c8a7b53e51df1cc2163418760a56c9b
İnanç Gümüş <m@inanc.io>
2024-10-13 22:42:44 -0400

    Listing 5.1 Printing the logo and usage message

---
+++ hit/cmd/hit/hit.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+const logo = `
+ __  __     __     ______
+/\ \_\ \   /\ \   /\__  _\
+\ \  __ \  \ \ \  \/_/\ \/
+ \ \_\ \_\  \ \_\    \ \_\
+  \/_/\/_/   \/_/     \/_/`
+
+const usage = `
+Usage:
+  -url
+       HTTP server URL (required)
+  -n
+       Number of requests
+  -c
+       Concurrency level
+  -rps
+       Requests per second`
+
+func main() {
+	fmt.Printf("%s\n%s", logo, usage)
+
+	/* TODO: integrate package hit */
+}
