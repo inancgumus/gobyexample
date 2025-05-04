0d31b110cbc35c0a9887223b7de51a10130b1851
İnanç Gümüş <m@inanc.io>
2025-05-04 16:19:30 -0400

    Listing 2.3 Testing the Parse function

---
+++ url/url_test.go
@@ -0,0 +1,18 @@
+package url
+
+import "testing"
+
+func TestParse(t *testing.T) {
+	const uri = "https://github.com/inancgumus"
+
+	got, err := Parse(uri)
+	if err != nil {
+		t.Fatalf("Parse(%q) err = %q, want <nil>", uri, err)
+	}
+	want := &URL{
+		Scheme: "https", Host: "github.com", Path: "inancgumus",
+	}
+	if *got != *want {
+		t.Errorf("Parse(%q)\ngot  %#v\nwant %#v", uri, got, want)
+	}
+}
