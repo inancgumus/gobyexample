ae727226ce622861d5a9ea70fa12866f284d394d
İnanç Gümüş <m@inanc.io>
2025-05-05 17:21:44 -0400

    Listing 5.2 Refactoring the main function

--- hit/cmd/hit/hit.go
+++ hit/cmd/hit/hit.go
@@ -21,12 +21,24 @@ type env struct {
 }
 
 func main() {
+	if err := run(&env{
+		stdout: os.Stdout,
+		stderr: os.Stderr,
+		args:   os.Args,
+	}); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(e *env) error {
 	c := config{
 		n: 100,
 		c: 1,
 	}
-	if err := parseArgs(&c, os.Args[1:]); err != nil {
-		os.Exit(1)
+	if err := parseArgs(&c, e.args[1:], e.stderr); err != nil {
+		return err
 	}
-	fmt.Printf("%s\n\nSending %d requests to %q (concurrency: %d)\n", logo, c.n, c.url, c.c)
+	fmt.Fprintf(e.stdout, "%s\n\nSending %d requests to %q (concurrency: %d)\n", logo, c.n, c.url, c.c)
+
+	return nil
 }
