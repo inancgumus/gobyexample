7992f1a64b6f4dd4c139aec09b1f140c8720920d
İnanç Gümüş <m@inanc.io>
2025-05-05 14:19:32 -0400

    Listing 4.6 Parsing with FlagSet

--- hit/cmd/hit/config.go
+++ hit/cmd/hit/config.go
@@ -1,10 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-	"strings"
-)
+import "flag"
 
 type config struct {
 	url string
@@ -13,49 +9,16 @@ type config struct {
 	rps int
 }
 
-type parseFunc func(string) error
-
 func parseArgs(c *config, args []string) error {
-	flagSet := map[string]parseFunc{
-		"url": stringVar(&c.url),
-		"n":   intVar(&c.n),
-		"c":   intVar(&c.c),
-		"rps": intVar(&c.rps),
-	}
+	fs := flag.NewFlagSet(
+		"hit",
+		flag.ContinueOnError,
+	)
 
-	for _, arg := range args {
-		name, val, _ := strings.Cut(arg, "=")
-		name = strings.TrimPrefix(name, "-")
+	fs.StringVar(&c.url, "url", "", "HTTP server `URL` (required)")
+	fs.IntVar(&c.n, "n", c.n, "Number of requests")
+	fs.IntVar(&c.c, "c", c.c, "Concurrency level")
+	fs.IntVar(&c.rps, "rps", c.rps, "Requests per second")
 
-		setVar, ok := flagSet[name]
-		if !ok {
-			return fmt.Errorf(
-				"flag provided but not defined: -%s",
-				name,
-			)
-		}
-		if err := setVar(val); err != nil {
-			return fmt.Errorf(
-				"invalid value %q for flag -%s: %w",
-				val, name, err,
-			)
-		}
-	}
-
-	return nil
-}
-
-func stringVar(p *string) parseFunc {
-	return func(s string) error {
-		*p = s
-		return nil
-	}
-}
-
-func intVar(p *int) parseFunc {
-	return func(s string) error {
-		var err error
-		*p, err = strconv.Atoi(s)
-		return err
-	}
+	return fs.Parse(args)
 }
