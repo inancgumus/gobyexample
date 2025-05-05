6cf376591a8b43046509289c27c2b5ef2aa28ac1
İnanç Gümüş <m@inanc.io>
2025-05-05 12:16:26 -0400

    Listing 3.11 Parallel tests

---
+++ url/parallel_test.go
@@ -0,0 +1,19 @@
+package url
+
+import (
+	"testing"
+	"time"
+)
+
+func TestParallelOne(t *testing.T) {
+	t.Parallel()
+	time.Sleep(5 * time.Second)
+}
+
+func TestParallelTwo(t *testing.T) {
+	t.Parallel()
+	time.Sleep(5 * time.Second)
+}
+
+func TestSequential(t *testing.T) {
+}
