3a8d31bf19d99c57c890b0bc2841d78bceb7513f
İnanç Gümüş <m@inanc.io>
2025-05-05 14:15:14 -0400

    Listing 4.3 Flag parser

--- hit/cmd/hit/config.go
+++ hit/cmd/hit/config.go
@@ -1,8 +1,49 @@
 package main
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type config struct {
+	url string
+	n   int
+	c   int
+	rps int
+}
 
 type parseFunc func(string) error
+
+func parseArgs(c *config, args []string) error {
+	flagSet := map[string]parseFunc{
+		"url": stringVar(&c.url),
+		"n":   intVar(&c.n),
+		"c":   intVar(&c.c),
+		"rps": intVar(&c.rps),
+	}
+
+	for _, arg := range args {
+		name, val, _ := strings.Cut(arg, "=")
+		name = strings.TrimPrefix(name, "-")
+
+		setVar, ok := flagSet[name]
+		if !ok {
+			return fmt.Errorf(
+				"flag provided but not defined: -%s",
+				name,
+			)
+		}
+		if err := setVar(val); err != nil {
+			return fmt.Errorf(
+				"invalid value %q for flag -%s: %w",
+				val, name, err,
+			)
+		}
+	}
+
+	return nil
+}
 
 func stringVar(p *string) parseFunc {
 	return func(s string) error {
