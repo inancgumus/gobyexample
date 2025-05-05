073659e0c724ceef759cc6e18c948d978c564b4f
İnanç Gümüş <m@inanc.io>
2025-05-05 18:40:09 -0400

    Listing 5.6 Test runner helper

--- hit/cmd/hit/hit_test.go
+++ hit/cmd/hit/hit_test.go
@@ -1,8 +1,21 @@
 package main
 
-import "strings"
+import (
+	"strings"
+)
 
 type testEnv struct {
 	stdout strings.Builder
 	stderr strings.Builder
+}
+
+func testRun(args ...string) (*testEnv, error) {
+	var tenv testEnv
+	err := run(&env{
+		args:   append([]string{"hit"}, args...),
+		stdout: &tenv.stdout,
+		stderr: &tenv.stderr,
+		dryRun: true,
+	})
+	return &tenv, err
 }
