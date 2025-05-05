0b3a842111ed08138910ea841972f90d5768d632
İnanç Gümüş <m@inanc.io>
2025-05-05 14:20:24 -0400

    Listing 4.7 Removing usage text

--- hit/cmd/hit/hit.go
+++ hit/cmd/hit/hit.go
@@ -12,24 +12,12 @@ const logo = `
  \ \_\ \_\  \ \_\    \ \_\
   \/_/\/_/   \/_/     \/_/`
 
-const usage = `  
-Usage:
-  -url
-       HTTP server URL (required)
-  -n
-       Number of requests
-  -c
-       Concurrency level
-  -rps
-       Requests per second`
-
 func main() {
 	c := config{
 		n: 100,
 		c: 1,
 	}
 	if err := parseArgs(&c, os.Args[1:]); err != nil {
-		fmt.Printf("%s\n%s", err, usage)
 		os.Exit(1)
 	}
 	fmt.Printf("%s\n\nSending %d requests to %q (concurrency: %d)\n", logo, c.n, c.url, c.c)
