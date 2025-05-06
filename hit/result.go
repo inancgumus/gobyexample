71cf27045aec7fa2f2a78071dc781a020a5a4202
İnanç Gümüş <m@inanc.io>
2025-05-06 11:11:12 -0400

    Listing 6.5 Summary type

--- hit/result.go
+++ hit/result.go
@@ -15,3 +15,16 @@ type Result struct {
 	Duration time.Duration // Duration is the time to complete a request
 	Error    error         // Error received after sending a request
 }
+
+// Summary is the summary of [Result] values.
+type Summary struct {
+	Started  time.Time     // Started is the time when requests began
+	Requests int           // Requests is the total number of requests made
+	Errors   int           // Errors is the total number of failed requests
+	Bytes    int64         // Bytes is the total number of bytes transferred
+	RPS      float64       // RPS is the number of requests sent per second
+	Duration time.Duration // Duration is the total time taken by requests
+	Fastest  time.Duration // Fastest is the fastest request duration
+	Slowest  time.Duration // Slowest is the slowest request duration
+	Success  float64       // Success is the ratio of successful requests
+}
