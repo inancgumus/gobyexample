85387dab7cf25d67b8e7428b7586df88ba4af3ea
İnanç Gümüş <m@inanc.io>
2025-05-06 11:49:06 -0400

    Listing 6.11 Integrating the HIT client

--- hit/cmd/hit/hit.go
+++ hit/cmd/hit/hit.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"net/http"
 	"os"
 	"time"
 
@@ -55,6 +56,20 @@ func run(e *env) error {
 }
 
 func runHit(c *config, stdout io.Writer) error {
+	req, err := http.NewRequest(http.MethodGet, c.url, http.NoBody)
+	if err != nil {
+		return fmt.Errorf("creating a new request: %w", err)
+	}
+	results, err := hit.SendN(c.n, req, hit.Options{
+		Concurrency: c.c,
+		RPS:         c.rps,
+	})
+	if err != nil {
+		return fmt.Errorf("sending requests: %w", err)
+	}
+
+	printSummary(hit.Summarize(results), stdout)
+
 	return nil
 }
 
