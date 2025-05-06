9be3c79849b859c2f11650f4ac3ccd52369f28ec
İnanç Gümüş <m@inanc.io>
2025-05-06 16:20:05 -0400

    Listing 7.7 Testing with a fake `RoundTripper`

--- hit/hit_test.go
+++ hit/hit_test.go
@@ -1,6 +1,9 @@
 package hit
 
-import "net/http"
+import (
+	"net/http"
+	"testing"
+)
 
 // roundTripperFunc is an adapter to allow the use of ordinary
 // functions as an [http.RoundTripper]. If the receiver f is a
@@ -10,4 +13,28 @@ type roundTripperFunc func(*http.Request) (*http.Response, error)
 
 func (f roundTripperFunc) RoundTrip(r *http.Request) (*http.Response, error) {
 	return f(r)
+}
+
+func TestSendStatusCode(t *testing.T) {
+	t.Parallel()
+
+	req, err := http.NewRequest(http.MethodGet, "/", http.NoBody)
+	if err != nil {
+		t.Fatalf("creating http request: %v", err)
+	}
+
+	fake := func(_ *http.Request) (*http.Response, error) {
+		return &http.Response{
+			StatusCode: http.StatusInternalServerError,
+		}, nil
+	}
+	client := &http.Client{
+		Transport: roundTripperFunc(fake),
+	}
+
+	result := Send(client, req)
+
+	if result.Status != http.StatusInternalServerError {
+		t.Errorf("got %d, want %d", result.Status, http.StatusInternalServerError)
+	}
 }
