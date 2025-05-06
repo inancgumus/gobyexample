4553396a87dd63d933dca09fe8b8e373fa8faea4
İnanç Gümüş <m@inanc.io>
2025-05-06 11:18:23 -0400

    Listing 6.6 Summarizing the results

--- hit/result.go
+++ hit/result.go
@@ -2,6 +2,7 @@ package hit
 
 import (
 	"iter"
+	"net/http"
 	"time"
 )
 
@@ -27,4 +28,39 @@ type Summary struct {
 	Fastest  time.Duration // Fastest is the fastest request duration
 	Slowest  time.Duration // Slowest is the slowest request duration
 	Success  float64       // Success is the ratio of successful requests
+}
+
+// Summarize returns a [Summary] of [Results].
+func Summarize(results Results) Summary {
+	var s Summary
+	if results == nil {
+		return s
+	}
+
+	started := time.Now()
+	for r := range results {
+		s.Requests++
+		s.Bytes += r.Bytes
+
+		if r.Error != nil || r.Status != http.StatusOK {
+			s.Errors++
+		}
+		if s.Fastest == 0 {
+			s.Fastest = r.Duration
+		}
+		if r.Duration < s.Fastest {
+			s.Fastest = r.Duration
+		}
+		if r.Duration > s.Slowest {
+			s.Slowest = r.Duration
+		}
+	}
+	if s.Requests > 0 {
+		s.Success = (float64(s.Requests-s.Errors) /
+			float64(s.Requests)) * 100
+	}
+	s.Duration = time.Since(started)
+	s.RPS = float64(s.Requests) / s.Duration.Seconds()
+
+	return s
 }
