a24c21b47de1a9c30fcb5ac5c868857470a68a54
İnanç Gümüş <m@inanc.io>
2025-05-06 13:06:58 -0400

    Listing 6.13 Integrating the producer stage

--- hit/pipe.go
+++ hit/pipe.go
@@ -2,6 +2,12 @@ package hit
 
 import "net/http"
 
+func runPipeline(n int, req *http.Request, opts Options) <-chan Result {
+	requests := produce(n, req)
+	_ = requests
+	return nil
+}
+
 func produce(n int, req *http.Request) <-chan *http.Request {
 	out := make(chan *http.Request)
 
