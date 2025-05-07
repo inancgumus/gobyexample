371455223afdd3e8e6d59b99ad13c1850eeddb16
İnanç Gümüş <m@inanc.io>
2025-05-07 19:29:03 -0400

    Listing 8.16 Adding a test helper

--- link/rest/health_test.go
+++ link/rest/health_test.go
@@ -1,6 +1,7 @@
 package rest
 
 import (
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -19,4 +20,15 @@ func TestHealth(t *testing.T) {
 	if got := rec.Body.String(); !strings.Contains(got, "OK") {
 		t.Errorf("\ngot body = %s\nwant contains %s", got, "OK")
 	}
+}
+
+// newRequest creates a new [http.Request] for testing.
+// It fails the calling test if it cannot create the request.
+func newRequest(tb testing.TB, method string, target string, body io.Reader) *http.Request {
+	tb.Helper()
+	r, err := http.NewRequest(method, target, body)
+	if err != nil {
+		tb.Fatalf("newRequest() err = %v, want nil", err)
+	}
+	return r
 }
