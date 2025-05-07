3da8133130b6793b4f4d09f1df9a2be9ab6bccf5
İnanç Gümüş <m@inanc.io>
2025-05-07 19:28:22 -0400

    Listing 8.15 Testing a handler

---
+++ link/rest/health_test.go
@@ -0,0 +1,22 @@
+package rest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestHealth(t *testing.T) {
+	t.Parallel()
+
+	rec := httptest.NewRecorder()
+	Health(rec, httptest.NewRequest(http.MethodGet, "/", nil))
+
+	if rec.Code != http.StatusOK {
+		t.Errorf("got status code = %d, want %d", rec.Code, http.StatusOK)
+	}
+	if got := rec.Body.String(); !strings.Contains(got, "OK") {
+		t.Errorf("\ngot body = %s\nwant contains %s", got, "OK")
+	}
+}
