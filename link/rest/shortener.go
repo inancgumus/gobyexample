258ed997c0b8170487762b18b38b73ad0f1ffc3c
İnanç Gümüş <m@inanc.io>
2025-05-07 19:14:36 -0400

    Listing 8.11 Resolve handler

--- link/rest/shortener.go
+++ link/rest/shortener.go
@@ -24,3 +24,19 @@ func Shorten(lg *slog.Logger, links *link.Shortener) http.Handler {
 		fmt.Fprint(w, key)
 	})
 }
+
+// Resolve returns an HTTP handler that resolves shortened link URLs.
+// It extracts a {key} from [http.Request] using [http.Request.PathValue].
+func Resolve(lg *slog.Logger, links *link.Shortener) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		lnk, err := links.Resolve(
+			r.Context(), link.Key(r.PathValue("key")),
+		)
+		if err != nil {
+			httpError(w, r, lg, fmt.Errorf("resolving: %w", err))
+			return
+		}
+
+		http.Redirect(w, r, lnk.URL, http.StatusFound)
+	})
+}
