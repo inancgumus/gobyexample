2c03d777cdec1d4c86cc505178e3c7e0d8513c18
İnanç Gümüş <m@inanc.io>
2025-05-12 13:57:34 -0400

    Listing 9.17 Error response helper

--- link/kit/hio/response.go
+++ link/kit/hio/response.go
@@ -1,5 +1,7 @@
 package hio
 
+import "fmt"
+
 // Responder provides helpers to write HTTP responses.
 type Responder struct {
 	err func(error) Handler
@@ -9,4 +11,9 @@ type Responder struct {
 // err is called when an error occurs during response writing.
 func NewResponder(err func(error) Handler) Responder {
 	return Responder{err: err}
+}
+
+// Error responds with a formatted error message.
+func (rs Responder) Error(format string, args ...any) Handler {
+	return rs.err(fmt.Errorf(format, args...))
 }
