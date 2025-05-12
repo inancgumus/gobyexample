eb5f532de6964aae5639c7bfa8d505062f0d030a
İnanç Gümüş <m@inanc.io>
2025-05-12 18:44:09 -0400

    Listing 9.23 Decoding JSON

---
+++ link/kit/hio/request.go
@@ -0,0 +1,29 @@
+package hio
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+// DecodeJSON reads and decodes JSON.
+//
+// It also validates the decoded value if it has the following method:
+//
+//	Validate() error
+func DecodeJSON(from io.Reader, to any) error {
+	data, err := io.ReadAll(from)
+	if err != nil {
+		return fmt.Errorf("reading: %w", err)
+	}
+	if err := json.Unmarshal(data, to); err != nil {
+		return fmt.Errorf("unmarshaling json: %w", err)
+	}
+	v, ok := to.(interface{ Validate() error })
+	if ok {
+		if err := v.Validate(); err != nil {
+			return fmt.Errorf("validating: %w", err)
+		}
+	}
+	return nil
+}
