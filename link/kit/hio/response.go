5de81e26e986f8c94834f2a2fac6d469314eaff5
İnanç Gümüş <m@inanc.io>
2025-05-12 13:57:57 -0400

    Listing 9.18 Redirect response helper

--- link/kit/hio/response.go
+++ link/kit/hio/response.go
@@ -1,6 +1,9 @@
 package hio
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 // Responder provides helpers to write HTTP responses.
 type Responder struct {
@@ -16,4 +19,12 @@ func NewResponder(err func(error) Handler) Responder {
 // Error responds with a formatted error message.
 func (rs Responder) Error(format string, args ...any) Handler {
 	return rs.err(fmt.Errorf(format, args...))
+}
+
+// Redirect redirects the request to the URL with the status code.
+func (rs Responder) Redirect(code int, url string) Handler {
+	return func(w http.ResponseWriter, r *http.Request) Handler {
+		http.Redirect(w, r, url, code)
+		return nil
+	}
 }
