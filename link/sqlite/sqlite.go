bb853896050170334faf345b5a6302a12ceceaeb
İnanç Gümüş <m@inanc.io>
2025-05-14 16:46:00 -0400

    Listing 10.6 Adding a test database helper

--- link/sqlite/sqlite.go
+++ link/sqlite/sqlite.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	_ "embed"
 	"fmt"
+	"testing"
 
 	_ "modernc.org/sqlite"
 )
@@ -25,4 +26,25 @@ func Dial(ctx context.Context, dsn string) (*sql.DB, error) {
 		return nil, fmt.Errorf("applying schema: %w", err)
 	}
 	return db, nil
+}
+
+// DialTestDB connects to a unique in-memory SQLite database.
+func DialTestDB(tb testing.TB) *sql.DB {
+	tb.Helper()
+
+	dsn := fmt.Sprintf(
+		"file:%s?mode=memory&cache=shared",
+		tb.Name(),
+	)
+	db, err := Dial(tb.Context(), dsn)
+	if err != nil {
+		tb.Fatalf("DialTestDB: %v", err)
+	}
+	tb.Cleanup(func() {
+		if err := db.Close(); err != nil {
+			tb.Logf("DialTestDB: closing: %v", err)
+		}
+	})
+
+	return db
 }
