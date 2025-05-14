de513b4d704ddde79e0f21f26cccd7e76192d543
İnanç Gümüş <m@inanc.io>
2025-05-14 17:45:12 -0400

    Listing 10.10 Resolving links

--- link/sqlite/shortener.go
+++ link/sqlite/shortener.go
@@ -3,6 +3,7 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/inancgumus/gobyexample/link"
@@ -39,4 +40,30 @@ func (s *Shortener) Shorten(ctx context.Context, lnk link.Link) (link.Key, error
 	}
 
 	return lnk.Key, nil
+}
+
+// Resolve resolves a [link.Link] by its [link.Key] from the database.
+func (s *Shortener) Resolve(ctx context.Context, key link.Key) (link.Link, error) {
+	if key.Empty() {
+		return link.Link{}, fmt.Errorf("validating: empty key: %w", link.ErrBadRequest)
+	}
+	if err := key.Validate(); err != nil {
+		return link.Link{}, fmt.Errorf("validating: %w: %w", err, link.ErrBadRequest)
+	}
+
+	// Retrieve the link from the database.
+	var uri string
+	err := s.db.QueryRowContext(
+		ctx,
+		`SELECT uri FROM links WHERE short_key = $1`,
+		key,
+	).Scan(&uri)
+	if errors.Is(err, sql.ErrNoRows) {
+		return link.Link{}, link.ErrNotFound
+	}
+	if err != nil {
+		return link.Link{}, fmt.Errorf("retrieving: %w: %w", err, link.ErrInternal)
+	}
+
+	return link.Link{Key: key, URL: uri}, nil
 }
