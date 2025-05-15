71a50cb803f51b0de97e8bf0094cecedbaa21a02
İnanç Gümüş <m@inanc.io>
2025-05-15 12:00:48 -0400

    Listing D.12 Notifying with multiple notifiers

--- oop/interfaces/main.go
+++ oop/interfaces/main.go
@@ -1,16 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	srv := &server{
-		url: "auth",
+		url:          "auth",
+		responseTime: time.Minute,
 	}
 	srv.check()
 	if !srv.slow() {
 		return
 	}
 	msg := fmt.Sprintf("%s server is slow: %s", srv.url, srv.responseTime)
-	notify(new(slackNotifier), msg)
-	notify(new(smsNotifier), msg)
+	notify(
+		multiNotifier{
+			new(slackNotifier),
+			new(smsNotifier),
+		},
+		msg,
+	)
 }
