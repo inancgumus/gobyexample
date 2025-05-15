19a6ede76ed04c60073f65b657bc2a94c5ddafe5
İnanç Gümüş <m@inanc.io>
2025-05-15 12:53:07 -0400

    Listing E.6 Sending and receiving

---
+++ concurrency/sendrecv/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func calculate() int { return 1 }
+
+func main() {
+	results := make(chan int)
+	for range 42 {
+		go func() { results <- calculate() }()
+	}
+	var total int
+	for range 42 {
+		total += <-results
+	}
+	fmt.Println("the meaning of universe:", total)
+}
