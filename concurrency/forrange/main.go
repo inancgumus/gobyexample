49da1a034ffa8dfb28f65576c87a227269bdea84
İnanç Gümüş <m@inanc.io>
2025-05-15 12:53:34 -0400

    Listing E.7 Closing a channel

---
+++ concurrency/forrange/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+func main() {
+	results := make(chan int)
+	go func() {
+		for n := range rand.N(100) + 1 {
+			results <- max(1, n*2)
+		}
+		close(results)
+	}()
+	for {
+		result, ok := <-results
+		if !ok {
+			break
+		}
+		fmt.Print(result, ".")
+	}
+}
