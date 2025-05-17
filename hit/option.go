41950eb31e23dcb1a3ce3c4bd74f829708651a72
İnanç Gümüş <m@inanc.io>
2025-05-16 22:34:32 -0400

    Listing F.2 Declaring options

--- hit/option.go
+++ hit/option.go
@@ -9,6 +9,29 @@ import (
 // [http.Request] and returns a [Result].
 type SendFunc func(*http.Request) Result
 
+// Option is a function that modifies the [Options].
+type Option func(o *Options)
+
+// MaxRetries sets the maximum number of retries for failed requests.
+func MaxRetries(n int) Option {
+	return func(o *Options) { o.MaxRetries = n }
+}
+
+// Concurrency sets the number of concurrent requests to send.
+func Concurrency(n int) Option {
+	return func(o *Options) { o.Concurrency = n }
+}
+
+// RPS sets the requests to send per second.
+func RPS(n int) Option {
+	return func(o *Options) { o.RPS = n }
+}
+
+// Sender sets the function to send requests.
+func Sender(f SendFunc) Option {
+	return func(o *Options) { o.Send = f }
+}
+
 // Options defines the options for sending requests.
 // Uses default options for unset options.
 type Options struct {
@@ -19,6 +42,10 @@ type Options struct {
 	// RPS is the requests to send per second.
 	// Default: 0 (no rate limiting)
 	RPS int
+
+	// MaxRetries is the maximum number of retries for failed requests.
+	// Default: 0 (no retries)
+	MaxRetries int
 
 	// Send processes requests.
 	// Default: Uses [Send].
@@ -33,6 +60,9 @@ func Defaults() Options {
 func withDefaults(o Options) Options {
 	if o.Concurrency == 0 {
 		o.Concurrency = 1
+	}
+	if o.MaxRetries == 0 {
+		o.MaxRetries = 5
 	}
 	if o.Send == nil {
 		client := &http.Client{
